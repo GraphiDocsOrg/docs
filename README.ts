aad40d432bf111ff32ef9d6264f6d91b1c23829a
Mitchell Simoens <mitchellsimoens@gmail.com>
2019-01-15 16:13:47 -0500

    Update README.ts

--- README.ts
+++ README.ts
@@ -1,10 +1,10 @@
-import * as Handlebars from "handlebars";
+import * as Handlebars from 'handlebars';
 import * as request from 'request';
 import * as util from 'util';
 
-import { readFile, writeFile } from "./lib/utility/fs";
+import { readFile, writeFile } from './lib/utility/fs';
 
-import { execSync } from "child_process";
+import { execSync } from 'child_process';
 
 Handlebars.registerHelper('bash', function (command: string) {
     return execSync(command).toString().replace(/\[\d{1,2}m/gi, '')
@@ -22,9 +22,10 @@ async function fromGithub (endpoint: string) {
 Promise
     .all([
         readFile('./README.handlebars', 'utf8'),
-        fromGithub('repos/2fd/graphdoc'),
-        fromGithub('repos/2fd/graphdoc/contributors').then(contributors => contributors.filter(c => c.login !== '2fd') ),
+        fromGithub('repos/GraphiDocsOrg/docs'),
+        fromGithub('repos/GraphiDocsOrg/docs/contributors')
+            .then(contributors => contributors.filter(c => c.login !== '2fd') ),
     ])
-    .then(([template, project, contributors]) => {
-        return writeFile('README.md',  Handlebars.compile(template)({project, contributors}))
-    })
+    .then(([template, project, contributors]) => 
+        writeFile('README.md',  Handlebars.compile(template)({project, contributors}))
+    )
